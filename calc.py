da8e680f0a48fdbe3a37b6cca04a6867b6f76406
Abdur Rauf Shah <abdurraufjaji@yahoo.com>
2024-06-06 05:49:22 +0500

    basics routes created

---
+++ calc.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template,request
+app = Flask (__name__)
+
+@app.route ('/', methods=["POST", "GET"])
+def calc():
+
+    if request.method =="POST":
+
+        n1 = float (request.form["num1"])
+        n2 = float (request.form["num2"])
+        Operations = request.form["Operations"]
+
+        if Operations == '+':
+             result = n1+n2
+
+        elif Operations == '-':
+            result = n1-n2
+
+        elif Operations == '*':
+            result = n1*n2
+
+        elif Operations == '/':
+            result = n1/n2
+        
+        else:
+            result = "Invalid Data Entered"
+        return render_template('calculator.html', result = result)
+    else:
+        return render_template('calculator.html')
+
+
+app.run(debug=True)
